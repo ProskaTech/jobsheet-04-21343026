eccfba403c4c3bf217040be02a305fdd7e6b9b9b
21343026 <112637349+21343026@users.noreply.github.com>
2022-09-15 19:31:59 +0700

    Add files via upload

---
+++ JS_4_Indah Chania_21343026/kondisiOperator1.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Indah Chania 21343026
+ * @author Indah 
+ */
+public class kondisiOperator1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        int score = 0;
+        char answer = 'a';
+        
+        score = (answer == 'b') ? 10 : 0;
+        System.out.println("Score = " + score);
+    }
+}
